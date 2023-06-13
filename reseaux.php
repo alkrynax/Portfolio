ac9e6402de1ff5ccd94765b9b3b6b1ba6395b569
alkrynax <alex@neoderme.com>
2023-06-13 10:47:30 +0200

    Ajout de la base de la page réseaux

---
+++ reseaux.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Portfolio Alexandre Deperdu Castel</title>
+</head>
+
+</html>
+<style>
+    body {
+         font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 20px;
+        background-color: #f1f1f1;
+    }
+
+    .container {
+        max-width: 768px;
+        margin: 0 auto;
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+        text-align: center;
+    }
+</style>
+</head>
+
+<body>
+</body>
+
+</html>
